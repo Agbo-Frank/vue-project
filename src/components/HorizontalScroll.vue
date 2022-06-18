1e18689ba3e5bcc2eeca5a8ca1510e4c5a008fbc
Agbo-Frank <agbofrank531@gmail.com>
2022-06-18 01:04:10 +0100

    First commit

--- src/components/HorizontalScroll.vue
+++ src/components/HorizontalScroll.vue
@@ -1,6 +1,20 @@
 <template>
-    <div>
-        Card2
+    <div class="w-48">
+        <div>
+            <p>Top Priority</p>
+            <p>My To-do list to finish before july</p>
+        </div>
+        <ol class="w-full rounded-3xl bg-grey-200 ring ring-grey-400 px-2 py-10 divide-y divide-dark-100">
+            <li class="flex self-start">
+                <p class="rounded-lg self-start  border border-solid border-black py-2 px-3 item">1</p>
+                <div>
+                    <p>Create a waireframe </p>
+                    <p>tody</p>
+                </div>
+            </li>
+            <li>guy</li>
+            <li>guy</li>
+        </ol>
     </div>
 </template>
 
